fd3bfa5082651cf929805ce549fcdaa9095064cf
Acekhing <charlesannorblay@gmail.com>
2022-08-18 14:33:13 +0000

    fix bug

--- functions/server.js
+++ functions/server.js
@@ -2,10 +2,12 @@ const functions = require("firebase-functions");
 const express = require('express');
 const bodyParser = require("body-parser");
 const request = require('request');
+const env = require('dotenv').config()
 
 const app = express();
 
-const URL = '';
+const BASE_URL = 'https://fcm.googleapis.com/fcm/send';
+const CONTENT_TYPE ='application/json';
 
 const router = express.Router();
 
@@ -14,11 +16,27 @@ app.use(bodyParser.json());
 
 router.post('/v2/send/user', (req, res) => {
 
+    const notification = req.body.notification;
+    const data = req.body.data;
+    const token = req.body.token;
+
+
+    const message = {
+        to: token,
+        dry_run: true,
+        data: data,
+        notification: {
+            title: notification.title,
+            body: notification.body,
+            click_action: 'intent-news-detail'
+        }
+    }
+
 });
 
 app.use('/api', router);
-app.listen(8085, ()=>{
-    console.log('Server started on PORT=8085');
+app.listen(env.PORT, ()=>{
+    console.log('Server started on PORT='+env.parsed.PORT);
 });
 
 exports.api = functions.https.onRequest((app));
